1e03a33f23393a7a1fa075d1218b90916a37e078
ts <torstenschenk22@yahoo.de>
2018-06-02 17:40:12 +0200

    added gstreamer dummy project

--- gstreamer-webpage-test/streaming-linux.sh
+++ gstreamer-webpage-test/streaming-linux.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 # for Linux
 
-gst-launch-1.0 \
-        v4l2src device=/dev/video0 \
-        ! videoconvert ! videoscale ! video/x-raw,width=320,height=240 \
-        ! clockoverlay shaded-background=true font-desc="Sans 38" \
-! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080
+#gst-launch-1.0 \
+#        v4l2src device=/dev/video0 \
+#        ! videoconvert ! videoscale ! video/x-raw,width=320,height=240 \
+#        ! clockoverlay shaded-background=true font-desc="Sans 38" \
+#! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080
+
+#gst-launch-1.0 -v v4l2src device="/dev/video0" ! videoconvert ! videoscale \
+#	! video/x-raw,width=300,height=300 \
+# 	!  theoraenc ! oggmux ! rtpjitterbuffer mode=0 !  tcpserversink host=127.0.0.1 port=8080  
+	
+gst-launch-1.0 -v videotestsrc ! vp8enc ! webmmux streamable=true name=stream ! \
+	tcpserversink host=localhost port=8080 	
+
+
+
---
+++ proj/main.cpp
@@ -0,0 +1,7 @@
+#include <gstreamer-1.0/gst/gst.h>
+
+int main (int argc, char *argv[])
+{
+  // initialize the GStreamer library
+  gst_init (&argc, &argv);
+}
